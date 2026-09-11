f8237b47596cfce915da26f0375bafbc8e707b5a
cltubigon <cltubigon@gmail.com>
2026-09-11 22:40:46 +0800

    fix(build): embed common controls v6 manifest

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,16 @@
 fn main() {
+    #[cfg(target_os = "windows")]
+    {
+        println!("cargo:rustc-link-arg=/MANIFEST:EMBED");
+        println!(
+            "cargo:rustc-link-arg=/MANIFESTDEPENDENCY:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'"
+        );
+
+        let windows = tauri_build::WindowsAttributes::new_without_app_manifest();
+        let attributes = tauri_build::Attributes::new().windows_attributes(windows);
+        tauri_build::try_build(attributes).expect("the Tauri build script should succeed");
+    }
+
+    #[cfg(not(target_os = "windows"))]
     tauri_build::build();
 }
